737f5360ed08ce15cff48a9e6b88b361b0bc9a69
Thaddaeus Johnson <tadd@rex-pro.com>
2019-05-01 19:17:20 -0600

    m

--- src/configurator.class.inc.php
+++ src/configurator.class.inc.php
@@ -5,8 +5,6 @@
  *  V5 is a large leap from V3 so V3 will not be obsoleted.
  */
 
-namespace library;
-
 class configurator {
 	use configurator_v5, configurator_v4 {
 		configurator_v5::domain_parts insteadof configurator_v4;
