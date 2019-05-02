bb88e2e800b90df956f6a35ade21105e7b206c4b
Thaddaeus Johnson <tadd@rex-pro.com>
2019-05-01 19:58:21 -0600

    remove extra stuff on file extension

---
+++ src/configurator.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ *  Introduction of Traits - update back to V3 to allow for skipping of V4 which is going to be rendered obsolete by V5
+ *  V5 is a large leap from V3 so V3 will not be obsoleted.
+ */
+
+class configurator {
+	use configurator_v5, configurator_v4 {
+		configurator_v5::domain_parts insteadof configurator_v4;
+	}
+}
